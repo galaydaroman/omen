736ee2b9ec9c0f0eacce356e4149d01bfb15577e
galaydaroman <galayda.roman@gmail.com>
2026-01-17 22:54:01 -0600

    FIX: Jumpy Debug page button

--- TODO.md
+++ TODO.md
@@ -3,10 +3,11 @@ TODO
 
 ## Bugs
 
-1. Jumpy Debug page button
+1. ~~Jumpy Debug page button~~
 
 ## Improvements
 
+1. PWA prompt update and UI
 1. NewEventLog buttons are too close to each other
 1. NewEventLog create action show show some confirmation notice
 1. Reset storage should have confirm dialog
--- src/HomeLayout.tsx
+++ src/HomeLayout.tsx
@@ -7,6 +7,40 @@ import CreateEventDrawer from '@/components/app/CreateEventDrawer'
 import DebugButton from '@/components/app/DebugButton'
 import './HomeLayout.css'
 
+export default function HomeLayout() {
+  const location = useLocation()
+  const isMobile = useIsMobile()
+
+  const isHome = useMemo(() => {
+    return location.pathname === '/'
+  }, [location.pathname])
+
+  const Wrapper = isMobile ? MotionWrapper : EmptyWrapper
+
+  return (
+    <Wrapper>
+      <div className="bg-background text-foreground min-h-screen">
+        <DebugButton />
+        <div className="flex justify-center space-between p-5">
+          <div className="flex-1 invisible"></div>
+          <div className="flex-2 text-center font-bold text-2xl">
+            <Link to="/">OMEN</Link>
+          </div>
+          <div className="flex flex-1 justify-end">
+            {
+              isHome && <CreateEventDrawer />
+            }
+          </div>
+        </div>
+        <Separator />
+        <div className="main">
+          <Outlet />
+        </div>
+      </div>
+    </Wrapper>
+  )
+}
+
 function MotionWrapper({ children }: React.PropsWithChildren) {
   const location = useLocation()
 
@@ -55,38 +89,4 @@ function EmptyWrapper({ children }: React.PropsWithChildren) {
   return <>
     {children}
   </>
-}
-
-export default function HomeLayout() {
-  const location = useLocation()
-  const isMobile = useIsMobile()
-
-  const isHome = useMemo(() => {
-    return location.pathname === '/'
-  }, [location.pathname])
-
-  const Wrapper = isMobile ? MotionWrapper : EmptyWrapper
-
-  return (
-    <Wrapper>
-      <div className="bg-background text-foreground">
-        <DebugButton />
-        <div className="flex justify-center space-between p-5">
-          <div className="flex-1 invisible"></div>
-          <div className="flex-2 text-center font-bold text-2xl">
-            <Link to="/">OMEN</Link>
-          </div>
-          <div className="flex flex-1 justify-end">
-            {
-              isHome && <CreateEventDrawer />
-            }
-          </div>
-        </div>
-        <Separator />
-        <div className="main">
-          <Outlet />
-        </div>
-      </div>
-    </Wrapper>
-  )
 }
